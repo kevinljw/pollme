df24e6dc46a375af0acbccef06ea0464007dd0f0
kevinljw <evin92@gmail.com>
2016-11-14 10:47:12 +0800

    renew

--- client/components/poll-form.js
+++ client/components/poll-form.js
@@ -1,31 +1,31 @@
 Template.pollForm.events({
 
   // handle the form submission
-  'submit form': function(event) {
+  // 'submit form': function(event) {
 
-    // stop the form from submitting
-    event.preventDefault();
+  //   // stop the form from submitting
+  //   event.preventDefault();
 
-    // get the data we need from the form
-    var newPoll = {
-      presenter: event.target.presenter.value,
-      project:  event.target.project.value,
-      demo: event.target.demo.value,
-      choices:[
-        { text: "有神快拜", votes: 0 },
-        { text: "好棒棒", votes: 0 },
-        { text: "還不錯", votes: 0 },
-        { text: "已閱畢", votes: 0 }
-      ],
-      ips: [],
-      score: 0,
-      people: 0,
-      now: false,
-      showed: false
-    };
+  //   // get the data we need from the form
+  //   var newPoll = {
+  //     presenter: event.target.presenter.value,
+  //     project:  event.target.project.value,
+  //     demo: event.target.demo.value,
+  //     choices:[
+  //       { text: "有神快拜", votes: 0 },
+  //       { text: "好棒棒", votes: 0 },
+  //       { text: "還不錯", votes: 0 },
+  //       { text: "已閱畢", votes: 0 }
+  //     ],
+  //     ips: [],
+  //     score: 0,
+  //     people: 0,
+  //     now: false,
+  //     showed: false
+  //   };
      
-    // create the new poll
-    Polls.insert(newPoll);
-  }
+  //   // create the new poll
+  //   Polls.insert(newPoll);
+  // }
 
 });
